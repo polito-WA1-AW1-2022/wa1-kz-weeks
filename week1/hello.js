ec079ea81ebde1df78d3160f9132cb907e580e17
Luigi De Russis <luigidr@gmail.com>
2022-03-01 19:08:13 +0100

    examples (1st lecture)

--- week1/hello.js
+++ week1/hello.js
@@ -1,1 +1,21 @@
-/* Start here */
+'strict mode';
+//let c = 8;
+//console.log(c);
+
+let a = 5;
+const b = '5';
+
+//a = 'hello';
+//b = 'another hello';
+
+console.log(a);
+console.log(b);
+
+if(a === b) {
+  console.log('a and b are equals');
+}
+else {
+  console.log('they are not equal');
+}
+
+
