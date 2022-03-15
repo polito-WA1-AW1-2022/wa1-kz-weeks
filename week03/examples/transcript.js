b0677501cb06623503497a723b9653938d1003fb
Luigi De Russis <luigidr@gmail.com>
2022-03-15 13:11:21 +0100

    code developed in class

--- week03/examples/transcript.js
+++ week03/examples/transcript.js
@@ -1,21 +1,24 @@
 'use strict';
 
 const sqlite = require('sqlite3');
+
 const db = new sqlite.Database('transcript.sqlite',
     (err) => { if (err) throw err; });
 
+let result = [];
 
-// let result = [];
 let sql = "SELECT * FROM course LEFT JOIN score ON course.code=score.coursecode" ;
 db.all(sql, (err,rows)=>{
     if(err) throw err;
     for (let row of rows) {
+        //console.log(row);
+        result.push(row);
+    }
+
+    console.log('*************');
+    for (let row of result) {
         console.log(row);
-        //result.push(row);
     }
 });
 
-// console.log('*************');
-// for (let row of result) {
-//     console.log(row);
-// }
+
--- week03/exercise.js
+++ week03/exercise.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const dayjs = require('dayjs');
+const sqlite = require('sqlite3');
 
 function Exam(code, name, credits, date, score, laude = false) {
   this.code = code;
   this.name = name;
   this.credits = credits;
-  this.date = date;
+  this.date = dayjs(date);
   this.score = score;
   this.laude = laude;
 
@@ -14,15 +15,32 @@ function Exam(code, name, credits, date, score, laude = false) {
 }
 
 function ExamList() {
+  const db = new sqlite.Database('exams.sqlite', err => { if (err) throw err;});
 
   // add
   this.add = (exam) => {
-    // write something clever
+    return new Promise((resolve, reject) => {
+      const sql = 'INSERT INTO score(coursecode, score, laude, datepassed) VALUES(?, ?, ?, DATE(?))';
+      db.run(sql, [exam.code, exam.score, exam.laude, exam.date.format('YYYY-MM-DD')], function(err) {
+        if(err) reject(err);
+        else resolve(this.lastID);
+      });
+    });
   };
 
   // getAll
   this.getAll = () => {
-    // write something clever
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM course JOIN score ON course.code = score.coursecode';
+      db.all(sql, [], (err, rows) => {
+        if(err)
+          reject(err);
+        else {
+          const exams = rows.map(row => new Exam(row.code, row.name, row.CFU, row.datepassed, row.score, (row.laude ? true : false)));
+          resolve(exams);
+        }
+      });
+    });
   };
 
   // find
@@ -43,8 +61,17 @@ function ExamList() {
 
 
 /* TESTING */
-const wa1 = new Exam('01TXYOV', 'Web Application I', 6, dayjs('2022-06-07'), 30, true);
-const softeng = new Exam('04GSPOV', 'Software Engineering I', 6, dayjs('2022-07-02'), 28);
+async function main() {
+  const examDb = new ExamList();
 
-const exams = new ExamList();
-exams.add(wa1);
+  const wa1 = new Exam('01TXYOV', 'Web Application I', 6, '2022-06-07', 30, true);
+  const softeng = new Exam('04GSPOV', 'Software Engineering I', 6, '2022-07-02', 28);
+
+  //const id = await examDb.add(wa1);
+  //console.log(id);
+
+  const myExams = await examDb.getAll();
+  console.log(`${myExams}`);
+}
+
+main();
