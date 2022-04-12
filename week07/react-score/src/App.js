02837f44f043aaaac0071cf5c6c081d2b49aab67
Luigi De Russis <luigidr@gmail.com>
2022-04-12 11:55:29 +0200

    example in class: state, delete, controlled form (beginning)

--- week07/react-score/src/App.js
+++ week07/react-score/src/App.js
@@ -2,7 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Container, Row, Col} from 'react-bootstrap';
 import dayjs from 'dayjs';
-import {ExamScores} from './components/ExamComponents';
+import ExamScores from './components/ExamComponents';
+import { useState } from 'react';
 
 const fakeExams = [
   {code: '01TYMOV', name: 'Information systems security', score: 30, date: dayjs('2022-02-01')},
@@ -11,15 +12,25 @@ const fakeExams = [
 ];
 
 function App() {
+  const [exams, setExams] = useState(fakeExams);
+
+  const deleteExam = (courseCode) => {
+    setExams((exs) => exs.filter(ex => ex.code !== courseCode));
+  }
+
+  const addExam = (exam) => {
+    setExams(oldExams => [...oldExams, exam]);
+  }
+
   return (
     <Container className='App'>
       <Row>
         <Col>
-          <h1>My Exams</h1>
+          <h1>My Exams ({exams.length})</h1>
         </Col>
       </Row>
       <Row>
-        <ExamScores exams={fakeExams}></ExamScores>
+        <ExamScores exams={exams} deleteExam={deleteExam} addExam={addExam}></ExamScores>
       </Row>
     </Container>
   );
--- week07/react-score/src/components/ExamComponents.js
+++ week07/react-score/src/components/ExamComponents.js
@@ -1,43 +1,55 @@
-import {Col, Table, Button} from 'react-bootstrap';
+import {Col, Table, Button, Form} from 'react-bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useState } from 'react';
+import dayjs from 'dayjs';
 
 function ExamScores(props) {
   return(
     <Col>
-      <ExamTable exams={props.exams}></ExamTable>
+      <ExamTable exams={props.exams} deleteExam={props.deleteExam} addExam={props.addExam}></ExamTable>
     </Col>
   );
 }
 
 function ExamTable(props) {
-  return(
-    <Table striped>
-      <thead>
-        <tr>
-          <th>Exam</th>
-          <th>Score</th>
-          <th>Date</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          props.exams.map((ex) => <ExamRow exam={ex} key={ex.code}/>)
-        }
-      </tbody>
-    </Table>
+  const [showForm, setShowForm] = useState(false);
+
+  return(<>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Code</th>
+            <th>Exam</th>
+            <th>Score</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.exams.map((ex) => <ExamRow exam={ex} key={ex.code}
+              deleteExam={props.deleteExam}
+            />)
+          }
+        </tbody>
+      </Table>
+      
+      {showForm ? <ExamForm addExam={props.addExam}/> :
+      <Button variant='success' onClick={() => setShowForm(true)}>Add</Button> }
+    </>
   );
 }
 
 function ExamRow(props) {
   return(
-    <tr><ExamData exam={props.exam}/><ExamActions /></tr>
+    <tr><ExamData exam={props.exam}/><ExamActions deleteExam={props.deleteExam} exam={props.exam}/></tr>
   );
 }
 
 function ExamData(props) {
   return(
     <>
+      <td>{props.exam.code}</td>
       <td>{props.exam.name}</td>
       <td>{props.exam.score}</td>
       <td>{props.exam.date.format('YYYY-MM-DD')}</td>
@@ -45,8 +57,42 @@ function ExamData(props) {
   );
 }
 
-function ExamActions() {
-  return <td><Button variant='danger'><i className='bi bi-trash3'></i></Button></td>
+function ExamActions(props) {
+  return <td><Button variant='danger' onClick={() => {props.deleteExam(props.exam.code)}}><i className='bi bi-trash3'></i></Button></td>
 }
 
-export {ExamScores};
+function ExamForm(props) {
+  const [code, setCode] = useState('');
+  const [course, setCourse] = useState('');
+  const [score, setScore] = useState(30);
+  const [date, setDate] = useState(dayjs());
+
+  return(
+    <Form>
+      <Form.Group>
+        <Form.Label>Course Code</Form.Label>
+        <Form.Control type="text" value={code} onChange={event => setCode(event.target.value)}/>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Course Name</Form.Label>
+        <Form.Control type="text" value={course} onChange={event => setCourse(event.target.value)}/>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Score</Form.Label>
+        <Form.Control type="number" value={score} onChange={event => setScore(event.target.value)}/>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Date</Form.Label>
+        <Form.Control type="date" value={date.format('YYYY-MM-DD')} onChange={event => setDate(event.target.value)}/>
+      </Form.Group>
+
+      <Button type="submit">Save</Button>
+
+    </Form>
+  )
+}
+
+export default ExamScores;
